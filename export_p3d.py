c5370dbd56896bd1b321e44444d54a1541bfbc44
Alexandre Boeuf <aboeuf@laas.fr>
2017-06-02 14:56:01 +0200

    Export 0.9.2

--- export_p3d.py
+++ export_p3d.py
@@ -1,8 +1,8 @@
 bl_info = {
     "name": "Export Move3D files",
     "author": "Alexandre Boeuf",
-    "version": (0, 9, 1),
-    "blender": (2, 78, 0),
+    "version": (0, 9, 2),
+    "blender": (2, 69, 0),
     "location": "File > Import-Export",
     "description": "Export Move3D files (.p3d/.macro)",
     "category": "Import-Export"}
@@ -13,12 +13,24 @@ from bpy_extras.io_utils import ExportHelper
 import os
 import math
 
-def meshesAreEquals(mA, mB):
+def meshesAreEquals(obA, obB, typeMesh):
 
-    ''' Compares vertices and polygons of two given meshes to determine if
-        they are geometrically the same i.e. if they have both the same
-        vertices and faces '''
-
+    ''' Compares vertices and polygons of the meshes of the two given objects to
+        determine if they are geometrically the same i.e. if they have both the
+        same vertices and faces '''
+    
+    if not obA or not obB or not obA.data or not obB.data:
+        return False
+        
+    mA = obA.data
+    mB = obB.data
+    
+    if type(mA) != typeMesh or type(mA) != typeMesh:
+        return False
+    
+    if obA == obB:
+        return True        
+    
     if mA == mB:
         return True
 
@@ -48,77 +60,112 @@ def meshesAreEquals(mA, mB):
         vA = mA.vertices[i]
         vB = mB.vertices[i]
         for j in range(3):
-            if vA.co[j] != vB.co[j]:
+            if obA.scale[j] * vA.co[j] != obB.scale[j] * vB.co[j]:
                 return False
 
     # Meshes are equal
     return True
 
-def sortMeshes(bpy):
+def sortObjects(bpy):
 
     ''' Return a dictionary with:
-            keys: the p3d name of geometrically equals meshes
-            values: a list of the geometrically equals meshes '''
+            keys: the p3d names of the geometrically equal meshes
+            values: a list of the objects having the same mesh '''
 
-    # Returns None if their are no meshes
-    if not bpy.data.objects:
+    # Returns None if there is no active scene (not sure if possible)
+    scene = bpy.context.scene
+    if not scene:
         return None
 
-    # First mesh is obviously always exported
-    name = bpy.data.meshes[0].name.lower().replace(" ","_").replace(".","_")
-    sortedMeshes = {name: [bpy.data.meshes[0]]}
+    # Returns None if there is no object in the scene
+    objects = scene.objects
+    if not objects:
+        return None
+    k = 0
+    while k < len(objects) and (not objects[k] or not objects[k].data or type(objects[k].data) != bpy.types.Mesh ): 
+        k = k + 1
+    ob = None
+    if k < len(objects):
+        ob = objects[k]
+    if not ob or not ob.data or type(ob.data) != bpy.types.Mesh:
+        return None
 
-    # For every following mesh in the scene ...
-    for i in range(1, len(bpy.data.meshes)):
+    # First object is added
+    mesh = ob.data
+    name = mesh.name.lower().replace(" ","_").replace(".","_")
+    sortedObjects = {name: [ob]}
+
+    # For every following object in the scene ...
+    for i in range(1, len(objects)):
         found = False
-        name = bpy.data.meshes[i].name.lower().replace(" ","_").replace(".","_")
-        # ... compare it to evey mesh we already stored ...
-        for item in sortedMeshes:
-            if meshesAreEquals(bpy.data.meshes[i], sortedMeshes[item][0]):
+        ob = objects[i]
+        if not ob or not ob.data or type(ob.data) != bpy.types.Mesh:
+            continue
+        name = ob.data.name.lower().replace(" ","_").replace(".","_")
+        # ... compare its mesh to every mesh we already stored ...
+        for item in sortedObjects:
+            if meshesAreEquals(ob, sortedObjects[item][0], bpy.types.Mesh):
                 # we have this one already
                 if len(name) < len(item):
                     # if it has a shorter name we update the key
-                    sortedMeshes[name] = sortedMeshes[item]
-                    del sortedMesh[item]
-                    # and we add it to sortedMeshes[name]
-                    sortedMeshes[name].append(bpy.data.meshes[i])
+                    sortedObjects[name] = sortedObjects[item]
+                    del sortedObjects[item]
+                    # and we add it to sortedObjects[name]
+                    sortedObjects[name].append(ob)
                 else:
-                    # otherwise we add it to sortedMeshes[item]
-                    sortedMeshes[item].append(bpy.data.meshes[i])
+                    # otherwise we add it to sortedObjects[item]
+                    sortedObjects[item].append(ob)
                 found = True
                 break
         #  ... and store it if need be in a new item
         if not found:
-            sortedMeshes[name] = [bpy.data.meshes[i]]
+            sortedObjects[name] = [ob]
 
-    return sortedMeshes
+    return sortedObjects
 
 def writeString(file, string):
     ''' Write string into a file opened in byte mode with UTF+8 encoding '''
     file.write(bytes(string,"UTF+8"))
 
-def exportMeshes(meshes, macrosPath):
+def exportMeshes(objects, macrosPath):
     ''' Export meshes as separate .macro files into the directory macrosPath '''
-    if not meshes:
+    if not objects:
         return
-    for item in meshes:
+    for item in objects:
+        if not objects[item]:
+            continue
+        if not objects[item][0]:
+            continue
+        mesh = objects[item][0].data
+        scale = objects[item][0].scale
+        if not mesh:
+            continue
+        vertices = mesh.vertices
+        if not vertices:
+            continue
+        polygons = mesh.polygons
+        if not polygons:
+            continue
         try:
             file = open(os.path.join(macrosPath, item + '.macro'), 'wb')
         except IOError:
             file = None
         if file:
-            if meshes[item] and meshes[item][0].vertices and meshes[item][0].polygons:
-                writeString(file, 'p3d_add_desc_poly '+item+'\n')
-                for v in meshes[item][0].vertices:
-                    writeString(file, '   p3d_add_desc_vert {0:.6f} {1:.6f} {2:.6f}\n'.format(v.co[0],v.co[1],v.co[2]))
+            writeString(file, 'p3d_add_desc_poly '+item+'\n')
+            for v in vertices:
+                writeString(file, '   p3d_add_desc_vert')
+                writeString(file, ' {0:.6f}'.format(scale[0]*v.co[0]))
+                writeString(file, ' {0:.6f}'.format(scale[1]*v.co[1]))
+                writeString(file, ' {0:.6f}'.format(scale[2]*v.co[2]))
                 writeString(file, '\n')
-                for f in meshes[item][0].polygons:
-                    if f.vertices:
-                        writeString(file, '   p3d_add_desc_face ')
-                        for i in f.vertices:
-                            writeString(file, '{0} '.format(i+1))
-                        writeString(file, '\n')
-                writeString(file, 'p3d_end_desc_poly\n')
+            writeString(file, '\n')
+            for f in polygons:
+                if f.vertices:
+                    writeString(file, '   p3d_add_desc_face ')
+                    for i in f.vertices:
+                        writeString(file, '{0} '.format(i+1))
+                    writeString(file, '\n')
+            writeString(file, 'p3d_end_desc_poly\n')
 
 def addObject(ob, macroName, filepath):
     if not ob:
@@ -154,25 +201,40 @@ def addObject(ob, macroName, filepath):
         else:
             writeString(file, '\n\n')
 
-def exportObjects(bpy, meshes, filepath):
+def exportObjects(objects, filepath):
     ''' Assemble all objects in .macro file referencing the other .macro files '''
-    if meshes:
+    if objects:
         try:
             file = open(filepath, 'wb')
         except IOError:
             file = None
         if file:
             file.close()
-            for item in meshes:
-                for m in meshes[item]:
-                    ob = bpy.data.objects[m.name]
-                    if ob:
-                        addObject(ob, item, filepath)
+            for item in objects:
+                for ob in objects[item]:
+                    addObject(ob, item, filepath)
+
+def logObjects(objects):
+    if objects:
+        try:
+            file = open('/home/aboeuf/BuildingYoan/log.txt', 'wb')
+        except IOError:
+            file = None
+        if file:
+            for item in objects:
+                writeString(file,'{0} -> [ '.format(item))
+                for ob in objects[item]:
+                    obName = ob.name.lower().replace(" ","_").replace(".","_")
+                    writeString(file,'{0} '.format(obName))
+                writeString(file,']\n');
+            file.close()
+                    
 
 def exportScene(bpy, dirname, filename):
-    meshes = sortMeshes(bpy)
-    exportMeshes(meshes, dirname)
-    exportObjects(bpy, meshes, os.path.join(dirname, filename))
+    objects = sortObjects(bpy)
+    logObjects(objects)
+    exportMeshes(objects, dirname)
+    exportObjects(objects, os.path.join(dirname, filename))
 
 class ExportP3D(bpy.types.Operator, ExportHelper):
     """Export scene or object to Move3D file(s)"""
